6aeaa921fff3740666696b174cf5e113ccbf1c7f
shortnd <ococncol@gmail.com>
2020-12-19 11:29:44 -0500

    application loggin

--- cmd/web/handlers.go
+++ cmd/web/handlers.go
@@ -3,35 +3,34 @@ package main
 import (
 	"fmt"
 	"html/template"
-	"log"
 	"net/http"
 	"strconv"
 )
 
-func home(writer http.ResponseWriter, request *http.Request)  {
+func (app *application) home(writer http.ResponseWriter, request *http.Request)  {
 	if request.URL.Path != "/" {
 		http.NotFound(writer, request)
 		return
 	}
 	files := []string{
-		"./ui/html/home.page.tmpl",
+		"./ui/html/home.page.bak",
 		"./ui/html/base.layout.tmpl",
 		"./ui/html/footer.partial.tmpl",
 	}
 	ts, err := template.ParseFiles(files...)
 	if err != nil {
-		log.Println(err.Error())
+		app.errorLog.Println(err.Error())
 		http.Error(writer, "Internal Server Error", 500)
 		return
 	}
 	err = ts.Execute(writer, nil)
 	if err != nil {
-		log.Println(err.Error())
+		app.errorLog.Println(err.Error())
 		http.Error(writer, "Internal Server Error", 500)
 	}
 }
 
-func showSnippet(writer http.ResponseWriter, request *http.Request)  {
+func (app *application) showSnippet(writer http.ResponseWriter, request *http.Request)  {
 	id, err := strconv.Atoi(request.URL.Query().Get("id"))
 	if err != nil {
 		http.NotFound(writer, request)
@@ -41,7 +40,7 @@ func showSnippet(writer http.ResponseWriter, request *http.Request)  {
 	fmt.Fprintf(writer, "Display a specific snippet with ID %d...", id)
 }
 
-func createSnippet(writer http.ResponseWriter, request *http.Request)  {
+func (app *application) createSnippet(writer http.ResponseWriter, request *http.Request)  {
 	if request.Method != http.MethodPost {
 		writer.Header().Set("Allow", http.MethodPost)
 		http.Error(writer, "Method Not Allowed", 405)
