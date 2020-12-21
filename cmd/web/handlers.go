bb6ef0fb90f76cb1f5a4e4f7829fc3f359d719b0
shortnd <ococncol@gmail.com>
2020-12-21 07:40:48 -0500

    about to start on dynamic templates

--- cmd/web/handlers.go
+++ cmd/web/handlers.go
@@ -1,8 +1,9 @@
 package main
 
 import (
+	"errors"
 	"fmt"
-	"html/template"
+	"github.com/shortnd/snippetbox/pkg/models"
 	"net/http"
 	"strconv"
 )
@@ -12,31 +13,50 @@ func (app *application) home(writer http.ResponseWriter, request *http.Request) 
 		app.notFound(writer)
 		return
 	}
-	files := []string{
-		"./ui/html/home.page.tmpl",
-		"./ui/html/base.layout.tmpl",
-		"./ui/html/footer.partial.tmpl",
-	}
-	ts, err := template.ParseFiles(files...)
+	s, err := app.snippets.Latest()
 	if err != nil {
 		app.serverError(writer, err)
 		return
 	}
-	err = ts.Execute(writer, nil)
-	if err != nil {
-		app.serverError(writer, err)
-		http.Error(writer, "Internal Server Error", 500)
+
+	for _, snippet := range s {
+		fmt.Fprintf(writer, "%v\n", snippet)
 	}
+
+	//return json.NewEncoder(writer).Encode(snippets)
+	//files := []string{
+	//	"./ui/html/home.page.tmpl",
+	//	"./ui/html/base.layout.tmpl",
+	//	"./ui/html/footer.partial.tmpl",
+	//}
+	//ts, err := template.ParseFiles(files...)
+	//if err != nil {
+	//	app.serverError(writer, err)
+	//	return
+	//}
+	//err = ts.Execute(writer, nil)
+	//if err != nil {
+	//	app.serverError(writer, err)
+	//}
 }
 
 func (app *application) showSnippet(writer http.ResponseWriter, request *http.Request) {
 	id, err := strconv.Atoi(request.URL.Query().Get("id"))
-	if err != nil {
+	if err != nil || id < 1 {
 		app.notFound(writer)
 		return
 	}
 
-	fmt.Fprintf(writer, "Display a specific snippet with ID %d...", id)
+	s, err := app.snippets.Get(id)
+	if err != nil {
+		if errors.Is(err, models.ErrNoRecord) {
+			app.notFound(writer)
+		} else {
+			app.serverError(writer, err)
+		}
+		return
+	}
+	fmt.Fprintf(writer, "%v", s)
 }
 
 func (app *application) createSnippet(writer http.ResponseWriter, request *http.Request) {
@@ -45,4 +65,15 @@ func (app *application) createSnippet(writer http.ResponseWriter, request *http.
 		app.clientError(writer, http.StatusMethodNotAllowed)
 		return
 	}
+	title := "0 snail"
+	content := "0 snail\nClimb Mount Fuji,\nBut slowly, slowly!,\n\n-Kobayashi Issa"
+	expires := "7"
+
+	id, err := app.snippets.Insert(title, content, expires)
+	if err != nil {
+		app.serverError(writer, err)
+		return
+	}
+
+	http.Redirect(writer, request, fmt.Sprintf("/snippet?id=%d", id), http.StatusSeeOther)
 }
